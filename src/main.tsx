f34d9c9dc2d076d464b531a206a5a7a7d744f033
Conri Turesson <50335159+RobynT98@users.noreply.github.com>
2025-10-17 15:09:08 +0200

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,35 +1,35 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-import App from "./App";
-import "./index.css";
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-import Home from "./pages/Home";
-import Sorg from "./pages/Sorg";
-import Minne from "./pages/Minne";
-import Bearbetning from "./pages/Bearbetning";
-import Dagbok from "./pages/Dagbok";
-import NotFound from "./pages/NotFound";
+import "@/index.css";
+import App from "@/App";
+
+import Home from "@/pages/Home";
+import Sorg from "@/pages/Sorg";
+import Minne from "@/pages/Minne";
+import Bearbetning from "@/pages/Bearbetning";
+import Journal from "@/pages/Journal";
+import Settings from "@/pages/Settings";
+import NotFound from "@/pages/NotFound";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
     children: [
-      { path: "/", element: <Home /> },
-      { path: "/sorg", element: <Sorg /> },
-      { path: "/minne", element: <Minne /> },
-      { path: "/bearbetning", element: <Bearbetning /> },
-      { path: "/dagbok", element: <Dagbok /> },
-      { path: "*", element: <NotFound /> }, // fångar ogiltiga vägar
+      { index: true, element: <Home /> },
+      { path: "sorg", element: <Sorg /> },
+      { path: "minne", element: <Minne /> },
+      { path: "bearbetning", element: <Bearbetning /> },
+      { path: "journal", element: <Journal /> },   // ⟵ fixad dagbokslänk
+      { path: "settings", element: <Settings /> },
+      { path: "*", element: <NotFound /> },        // fångar ogiltiga routes
     ],
   },
 ]);
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <RouterProvider router={router} />
   </React.StrictMode>
