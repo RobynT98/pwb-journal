d1f53de2e9c3cdb17c67eabda8021772440bd508
Conri Turesson <50335159+RobynT98@users.noreply.github.com>
2025-10-17 15:03:44 +0200

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,30 +1,35 @@
 import React from "react";
-import { createRoot } from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import "@/index.css";
+import ReactDOM from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import App from "./App";
+import "./index.css";
 
-import App from "@/App";
-import Home from "@/pages/Home";
-import Sorg from "@/pages/Sorg";
-import Minne from "@/pages/Minne";
-import Bearbetning from "@/pages/Bearbetning";
-import Journal from "@/pages/Journal";
+import Home from "./pages/Home";
+import Sorg from "./pages/Sorg";
+import Minne from "./pages/Minne";
+import Bearbetning from "./pages/Bearbetning";
+import Dagbok from "./pages/Dagbok";
+import NotFound from "./pages/NotFound";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
     children: [
-      { index: true, element: <Home /> },
-      { path: "sorg", element: <Sorg /> },
-      { path: "minne", element: <Minne /> },
-      { path: "bearbetning", element: <Bearbetning /> },
-      { path: "journal", element: <Journal /> },
+      { path: "/", element: <Home /> },
+      { path: "/sorg", element: <Sorg /> },
+      { path: "/minne", element: <Minne /> },
+      { path: "/bearbetning", element: <Bearbetning /> },
+      { path: "/dagbok", element: <Dagbok /> },
+      { path: "*", element: <NotFound /> }, // fångar ogiltiga vägar
     ],
   },
 ]);
 
-createRoot(document.getElementById("root")!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <RouterProvider router={router} />
   </React.StrictMode>
